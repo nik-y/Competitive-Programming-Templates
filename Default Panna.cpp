34c5c95bd06d63d6f536641ab5cbcfd78f718532
nik-y <48621840+nik-y@users.noreply.github.com>
2021-03-13 14:32:13 +0530

    syntax update

--- Default Panna.cpp
+++ Default Panna.cpp
@@ -62,27 +62,6 @@ int main() {
 }
 
 
-/////////////////// confidential ///////////////
-
-#include <bits/stdc++.h>
-using namespace std;
-
-using ll = long long; 
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    int tt;
-    cin >> tt;
-    while(tt --) {
-        
-    }
-}
-
-///////////////////////////////////
-
-
 
 ////////// HackerRank ///////////
 
